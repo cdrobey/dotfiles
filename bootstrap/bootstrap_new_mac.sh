4c738abc771bb17fbe0319acdc77874eaa10cbe3
Chris Roberson <cdroberson@gmail.com>
2019-11-15 22:51:04 -0700

    update.

--- bootstrap/bootstrap_new_mac.sh
+++ bootstrap/bootstrap_new_mac.sh
@@ -3,8 +3,8 @@
 # Run this on a stock Mac to bootstrap it with Nate's dotfiles and customizations
 #
 
-PROJECTS="~/Projects"
-DOTFILES="~/.dotfiles"
+DOTFILES=".dotfiles"
+
 
 # Ask for the administrator password upfront
 echo "Asking for your sudo password upfront..."
@@ -15,25 +15,23 @@ while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 
 # Install homebrew and git (xcode tools)
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-brew doctor
-
-# Create our code directory
-[[ -d "$PROJECTS" ]] || mkdir "$PROJECTS"
 
 # Install HomeBrew apps
-brew bundle --file=~"$DOTFILES"/boostrap/Brewfile
-brew cask cleanup
+brew bundle --file=~/"$DOTFILES"/bootstrap/Brewfile
 
 # Pin Ruby versions so I don't lose all my gems on upgrade.
 brew pin ruby
 brew pin ruby-build
 brew pin rbenv
 
-cd ~/."$DOTFILES"} && ./install
+cd ~/"$DOTFILES" && ./install
 
 # Run MACOS config script
 echo "Configuring a bunch of MACOS things"
 sh ~/"$DOTFILES"/bootstrap/macos.sh
+
+# Use  zsh
+chsh -s /bin/zsh
 
 echo
 echo "Finished!"
